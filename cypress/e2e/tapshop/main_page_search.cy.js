45fa9adc1832c15352fdc66161cae09247928c74
malkrysiak <malgorzata.krysiak@cluepoints.com>
2025-01-16 22:41:34 +0100

    update mispelling

--- cypress/e2e/tapshop/main_page_search.cy.js
+++ cypress/e2e/tapshop/main_page_search.cy.js
@@ -11,7 +11,7 @@ describe('Main Page Search', () => {
     cy.get('#woocommerce-product-search-field-0').click().type("Beanie").type('{enter}')
         cy.get("div h1:contains('Beanie')").should("be.visible")
   
-      // enter value that doesn;t exists
+      // enter value that doesn't exist
       cy.get('#woocommerce-product-search-field-0').click().type("gosia").type('{enter}')
         cy.get(".woocommerce-info").contains("Nie znaleziono produktów, których szukasz.").should("be.visible");
     });})
