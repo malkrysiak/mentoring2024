aed424d26d0a9b16f424cf0d358d2a78fe21aa0f
malkrysiak <malgorzata.krysiak@cluepoints.com>
2025-02-28 11:40:10 +0100

    check precommit

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
 console.log("this is my first test")
+
+cy.get("costam")
+                cy.get("costam")
+cy.get("costam")
+
+
+
+
+
+
+
+
+cy.get("costam")
+                                        cy.get("costam")
+cy.get("costam")
+cy.get("costam")
+cy.get("costam")
